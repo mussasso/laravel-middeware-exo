24b07f9ae3e965aaf55daf42a0493b0d35547b42
mussasso <darouich.mm@hotmail.com>
2022-11-03 14:35:05 +0100

    first push

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/acceuil', function () {
+    return view('pages.acceuil');
+})->middleware('Connected');
+Route::get('/article', function () {
+    return view('pages.article');
+})->middleware('Connected');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+require __DIR__.'/auth.php';
